daabe2d3aece4569dd8ed566cf2d05c073d199dd
mineorbit <maximilian.stillger@arcor.de>
2021-01-30 21:55:17 +0100

    Setup basic code structure

---
+++ Runtime/Level/Data/LevelObjectData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+    public class LevelObjectData : ScriptableObject
+    {
+
+    }
+}
