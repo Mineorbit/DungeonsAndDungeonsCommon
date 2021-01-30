a9643fa979e8b0ab3f0f6dcc5e3ce5ced8597985
mineorbit <maximilian.stillger@arcor.de>
2021-01-30 21:55:17 +0100

    Setup basic code structure

---
+++ Runtime/Level/Data/ChunkData.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+    public class ChunkData : ScriptableObject
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
