927b65c38f2227dcaef3d0d6a199c2ffdd1a5d5e
mineorbit <mineorbitbeta@gmail.com>
2021-04-07 23:31:19 +0200

    Fixed missing reference to Navmeshcomponents

--- Runtime/AI/LevelNavGenerator.cs
+++ Runtime/AI/LevelNavGenerator.cs
@@ -2,31 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
-public class LevelNavGenerator : MonoBehaviour
+namespace com.mineorbit.dungeonsanddungeonscommon
 {
-    public static NavMeshSurface navMeshSurface;
-    void Start()
+    public class LevelNavGenerator : MonoBehaviour
     {
-        if(navMeshSurface == null) navMeshSurface = GetComponent<NavMeshSurface>();
+        public static NavMeshSurface navMeshSurface;
+        void Start()
+        {
+            if (navMeshSurface == null) navMeshSurface = GetComponent<NavMeshSurface>();
 
-    }
+        }
 
-    public static void UpdateNavMesh()
-    {
-    Debug.Log("Building");
-    Enemy[] enemies = Level.GetAllEnemies();
-    foreach(Enemy e in enemies)
-    {
-            if(e != null)
-            e.gameObject.SetActive(false);
-    }
+        public static void UpdateNavMesh()
+        {
+            Debug.Log("Building");
+            Enemy[] enemies = Level.GetAllEnemies();
+            foreach (Enemy e in enemies)
+            {
+                if (e != null)
+                    e.gameObject.SetActive(false);
+            }
 
-    navMeshSurface.BuildNavMesh();
+            navMeshSurface.BuildNavMesh();
 
-    foreach (Enemy e in enemies)
-    {
-            if (e != null)
-                e.gameObject.SetActive(true);
-    }
+            foreach (Enemy e in enemies)
+            {
+                if (e != null)
+                    e.gameObject.SetActive(true);
+            }
+        }
     }
 }
