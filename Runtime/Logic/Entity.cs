ef10cbd53235507222a8c486303bcf0ab8b288a1
mineorbit <mineorbitbeta@gmail.com>
2021-04-08 00:37:47 +0200

    more restructure and moving of logic

--- Runtime/Logic/Entity.cs
+++ Runtime/Logic/Entity.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 namespace com.mineorbit.dungeonsanddungeonscommon
 {
-    public class Entity : MonoBehaviour
+    public class Entity : LevelObject
     {
         public int health;
         // Start is called before the first frame update
