e6623c4caa90513ae43d8a66767083869db935eb
mineorbit <mineorbitbeta@gmail.com>
2021-04-08 13:01:10 +0200

    New Restructure

---
+++ Runtime/Item/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+
+    public class Item : MonoBehaviour
+    {
+        public GameObject owner;
+        public virtual void OnAttach()
+        {
+            owner = GetComponentInParent<Player>().gameObject;
+        }
+        public virtual void OnDettach()
+        {
+
+        }
+
+        public virtual void Use()
+        {
+
+        }
+
+        public virtual void StopUse()
+        {
+
+        }
+    }
+}
