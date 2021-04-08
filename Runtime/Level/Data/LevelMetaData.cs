b2ccce0c43ea98e16db38030a4ccb2e53cafdb1d
mineorbit <mineorbitbeta@gmail.com>
2021-04-08 16:06:52 +0200

    Add meta data fields

--- Runtime/Level/Data/LevelMetaData.cs
+++ Runtime/Level/Data/LevelMetaData.cs
@@ -8,6 +8,11 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         public int localLevelId;
         public long uniqueLevelId;
 		public string FullName;
+		
+		public bool availBlue;
+		public bool availYellow;
+		public bool availRed;
+		public bool availGreen;
         
 		public LevelMetaData(string name){
 		FullName = name;
