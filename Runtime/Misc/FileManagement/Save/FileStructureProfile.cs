33cdd0611e877853657a3cc9325365c575f3d5df
mineorbit <mineorbitbeta@gmail.com>
2021-04-12 15:21:15 +0200

    Moved file management

---
+++ Runtime/Misc/FileManagement/Save/FileStructureProfile.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/FileStructureProfile", order = 1)]
+    public class FileStructureProfile : ScriptableObject
+    {
+        public string name;
+        public FileStructureProfile[] subStructures;
+    }
+}
