91f075206e1ab627da5391ca9d5a00244832a0a0
mineorbit <mineorbitbeta@gmail.com>
2021-04-12 15:53:35 +0200

    Fixed naming

--- Runtime/Misc/FileManagement/FileStructureProfile.cs
+++ Runtime/Misc/FileManagement/FileStructureProfile.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/FileStructureProfile", order = 1)]
-public class FileStructureProfile : ScriptableObject
+namespace com.mineorbit.dungeonsanddungeonscommon
 {
-    public string name;
-    public FileStructureProfile[] subStructures;
+    [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/FileStructureProfile", order = 1)]
+    public class FileStructureProfile : ScriptableObject
+    {
+        public string name;
+        public FileStructureProfile[] subStructures;
+    }
 }
