ce888caf62f906ef0311489d3a57c1172cc45872
mineorbit <mineorbitbeta@gmail.com>
2021-04-17 16:25:26 +0200

    Added Rotation support

--- Runtime/Misc/FileManagement/Save/SaveManager.cs
+++ Runtime/Misc/FileManagement/Save/SaveManager.cs
@@ -5,84 +5,86 @@ using UnityEngine;
 using System.IO;
 using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
-
-public class SaveManager
+namespace com.mineorbit.dungeonsanddungeonscommon
 {
-    public enum StorageType { BIN, XML, JSON };
-    StorageType storageType;
-    public SaveManager(StorageType t)
+    public class SaveManager
     {
-        storageType = t;
-    }
-    public T Load<T>(string path)
-    {
-        T result = default(T);
-        if (!File.Exists(path)) return result;
-
-        if (storageType == StorageType.JSON)
-        { 
-        StreamReader reader = new StreamReader(path);
-        string data = reader.ReadToEnd();
-        reader.Close();
-        result = JsonUtility.FromJson<T>(data);
-        }
-        else if(storageType == StorageType.BIN)
+        public enum StorageType { BIN, XML, JSON };
+        StorageType storageType;
+        public SaveManager(StorageType t)
         {
-            FileStream fs = new FileStream(path, FileMode.Open);
-            try
-            {
-                BinaryFormatter formatter = new BinaryFormatter();
+            storageType = t;
+        }
+        public T Load<T>(string path)
+        {
+            T result = default(T);
+            if (!File.Exists(path)) return result;
 
-                // Deserialize the hashtable from the file and
-                // assign the reference to the local variable.
-                result = (T) formatter.Deserialize(fs);
-            }
-            catch (SerializationException e)
+            if (storageType == StorageType.JSON)
             {
-                UnityEngine.Debug.Log("Failed to deserialize. Reason: " + e.Message);
-                throw;
+                StreamReader reader = new StreamReader(path);
+                string data = reader.ReadToEnd();
+                reader.Close();
+                result = JsonUtility.FromJson<T>(data);
             }
-            finally
+            else if (storageType == StorageType.BIN)
             {
-                fs.Close();
+                FileStream fs = new FileStream(path, FileMode.Open);
+                try
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+
+                    // Deserialize the hashtable from the file and
+                    // assign the reference to the local variable.
+                    result = (T)formatter.Deserialize(fs);
+                }
+                catch (SerializationException e)
+                {
+                    UnityEngine.Debug.Log("Failed to deserialize. Reason: " + e.Message);
+                    throw;
+                }
+                finally
+                {
+                    fs.Close();
+                }
+                return result;
             }
+
             return result;
         }
-
-        return result;
-    }
-    public bool Save(object o,string path, bool persistent = true)
+        public bool Save(object o, string path, bool persistent = true)
         {
-        string filePath = (persistent ? Application.persistentDataPath : "") + path;
-        if(storageType == StorageType.JSON)
-        { 
-        StreamWriter writer = new StreamWriter(filePath);
-        string content = JsonUtility.ToJson(o);
-        writer.WriteLine(content);
-        writer.Flush();
-        writer.Close();
+            string filePath = (persistent ? Application.persistentDataPath : "") + path;
+            if (storageType == StorageType.JSON)
+            {
+                StreamWriter writer = new StreamWriter(filePath);
+                string content = JsonUtility.ToJson(o);
+                writer.WriteLine(content);
+                writer.Flush();
+                writer.Close();
 
+            }
+            else if (storageType == StorageType.BIN)
+            {
+                FileStream fs = new FileStream(filePath, FileMode.Create);
+
+                BinaryFormatter formatter = new BinaryFormatter();
+                try
+                {
+                    formatter.Serialize(fs, o);
+                }
+                catch (SerializationException e)
+                {
+                    UnityEngine.Debug.Log("Failed to serialize. Reason: " + e.Message);
+                    throw;
+                }
+                finally
+                {
+                    fs.Close();
+                }
+            }
+
+            return true;
         }
-        else if(storageType == StorageType.BIN)
-        {
-            FileStream fs = new FileStream(filePath, FileMode.Create);
-
-            BinaryFormatter formatter = new BinaryFormatter();
-            try
-            {
-                formatter.Serialize(fs, o);
-            }
-            catch (SerializationException e)
-            {
-                UnityEngine.Debug.Log("Failed to serialize. Reason: " + e.Message);
-                throw;
-            }
-            finally
-            {
-                fs.Close();
-            }
-        }
-
-        return true;
     }
 }
