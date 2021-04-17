8020abcd571c2eed256c879f2e914afc83457f0d
mineorbit <mineorbitbeta@gmail.com>
2021-04-17 16:25:26 +0200

    Added Rotation support

---
+++ Runtime/Play/PlayerBaseAnimator.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+    public class PlayerBaseAnimator : MonoBehaviour
+    {
+        public CharacterController characterController;
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Vector3 forwardDirection = characterController.velocity;
+            float angleY = Mathf.Atan2(forwardDirection.x,forwardDirection.z);
+
+            transform.eulerAngles = (new Vector3(0,angleY,0));
+        }
+    }
+}
