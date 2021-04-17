661e378e6b584bb6ed55a6f1ed7e6d2cee990463
mineorbit <mineorbitbeta@gmail.com>
2021-04-17 18:40:31 +0200

    Made interpolation smooth

--- Runtime/Play/PlayerBaseAnimator.cs
+++ Runtime/Play/PlayerBaseAnimator.cs
@@ -7,16 +7,17 @@ namespace com.mineorbit.dungeonsanddungeonscommon
     public class PlayerBaseAnimator : MonoBehaviour
     {
         public CharacterController characterController;
+        Vector3 forwardDirection;
         void Start()
         {
-
+            forwardDirection = new Vector3(0,0,0);
         }
 
         // Update is called once per frame
         void Update()
         {
-            Vector3 forwardDirection = characterController.velocity;
-            float angleY = (180/Mathf.PI) * Mathf.Atan2(forwardDirection.x,forwardDirection.z);
+            forwardDirection =  (forwardDirection + characterController.velocity)/2;
+            float angleY = 180 + (180/Mathf.PI) * Mathf.Atan2(forwardDirection.x,forwardDirection.z);
 
             transform.eulerAngles = (new Vector3(0,angleY,0));
         }
