43e1a294dcd220c84e1956218984b35852e88c91
mineorbit <mineorbitbeta@gmail.com>
2021-04-20 11:21:44 +0200

    fixed player goal

--- Runtime/Level/Structure/LevelObject/PlayerGoal.cs
+++ Runtime/Level/Structure/LevelObject/PlayerGoal.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace com.mineorbit.dungeonsanddungeonscommon
 {
     public class PlayerGoal : LevelObject
     {
+        public static UnityEvent GameWinEvent = new UnityEvent();
+
+        bool[] playersInside;
+
         // Start is called before the first frame update
         public override void OnInit()
         {
             if (LevelManager.currentLevel.goal != null)
                 LevelManager.currentLevel.Remove(this);
             LevelManager.currentLevel.goal = this;
+        }
+
+
+        void OnEnable()
+        {
+            playersInside = new bool[4];
         }
 
         // Update is called once per frame
@@ -22,7 +33,18 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 
         void OnTriggerEnter(Collider other)
         {
-            Debug.Log("Test");
+            int p = PlayerManager.GetPlayerId(other.gameObject);
+            if (p > 0) playersInside[p] = true;
+            for (int i = 0; i < 4; i++)
+            {
+                if (!playersInside[i]) return;
+            }
+            GameWinEvent.Invoke();
+        }
+        void OnTriggerExit(Collider other)
+        {
+            int p = PlayerManager.GetPlayerId(other.gameObject);
+            if (p > 0) playersInside[p] = false;
         }
     }
 }
