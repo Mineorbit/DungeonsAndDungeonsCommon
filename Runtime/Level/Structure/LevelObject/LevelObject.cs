bc9a3ed4a232366e532beac8a248546257b96cdc
mineorbit <mineorbitbeta@gmail.com>
2021-04-23 23:20:15 +0200

    level reset finally working

--- Runtime/Level/Structure/LevelObject/LevelObject.cs
+++ Runtime/Level/Structure/LevelObject/LevelObject.cs
@@ -10,6 +10,9 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 
 		bool initialized = false;
 
+        // Initializes a LevelObject to its original state
+        // It must be assured that this also resets a LevelObject to its Initial State in the Level (If Static that is  self explanatory, might cause
+        // destruction of some dynamic LevelObjects aswell, if Dynamic may not cause the distruction of static LevelObjects (but may involve other dynamic GameObjects ))
         public virtual void OnInit()
         {
 			initialized = true;
@@ -23,7 +26,6 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         public virtual void Reset()
         {
             Debug.Log("Resetting "+levelObjectDataType);
-            OnDeInit();
             OnInit();
         }
         
