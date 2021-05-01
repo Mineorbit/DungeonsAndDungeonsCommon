ce1b89b99b868aac5d33bd791a5ab2904406da30
mineorbit <mineorbitbeta@gmail.com>
2021-05-01 11:59:05 +0200

    fix

---
+++ Runtime/Network/Server.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Server
+{
+
+    public Server()
+    {
+
+    }
+
+    public static void Disconnect(int localId)
+    {
+
+    }
+
+    public void Start()
+    {
+
+    }
+
+    public void Stop()
+    {
+
+    }
+
+    public void DisconnectAll()
+    {
+
+    }
+
+    public void StopListen()
+    {
+
+    }
+}
