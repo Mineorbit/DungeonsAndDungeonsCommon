9f55f5c18b239296527c7f7e2a8604d49a3a3f51
mineorbit <maximilian.stillger@arcor.de>
2021-05-08 21:49:24 +0200

    fixes

--- Runtime/Level/Manager/LevelLoadTarget.cs
+++ Runtime/Level/Manager/LevelLoadTarget.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LevelLoadTarget : MonoBehaviour
+namespace com.mineorbit.dungeonsanddungeonscommon
 {
-    // Start is called before the first frame update
-    void Start()
+    public class LevelLoadTarget : NetworkLevelObject
     {
-        
-    }
+        // Start is called before the first frame update
+        void Start()
+        {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
     }
 }
