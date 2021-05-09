922b7b61a8e708d34deb138decff0848e9b03ea0
mineorbit <maximilian.stillger@arcor.de>
2021-05-09 22:50:16 +0200

    fix

--- Runtime/Level/Manager/LevelLoadTarget.cs
+++ Runtime/Level/Manager/LevelLoadTarget.cs
@@ -24,6 +24,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
                 { 
                     Invoke(StreamChunkIntoCurrentLevelFrom,chunkData);
                 }
+                loadedLocalChunks.Add(ChunkManager.GetChunkGridPosition(position));
             }
         }
 
