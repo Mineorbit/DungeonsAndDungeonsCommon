8bb672ed7901f1607e1629b51b69b099ca409b02
mineorbit <mineorbitbeta@gmail.com>
2021-05-12 22:00:17 +0200

    fix

--- Runtime/Misc/MainCaller.cs
+++ Runtime/Misc/MainCaller.cs
@@ -7,11 +7,28 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 {
     public class MainCaller : MonoBehaviour
     {
+
+        public static MainCaller instance;
+
+        public void Start()
+        {
+            if(instance != null)
+            {
+                Destroy(this);
+            }
+            instance = this;
+        }
+
         public static Queue<Action> todo = new Queue<Action>();
 
         public static void Do(Action a)
         {
             todo.Enqueue(a);
+        }
+
+        public static void startCoroutine(IEnumerator c)
+        {
+            instance.StartCoroutine(c);
         }
 
         void Update()
