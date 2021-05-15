3a5ffb4b366a763d1fb392cba657ecaec0fcf480
mineorbit <mineorbitbeta@gmail.com>
2021-05-15 12:50:09 +0200

    fix

---
+++ Runtime/Network/NetworkHandlers/ItemNetworkHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+    public class ItemNetworkHandler : LevelObjectNetworkHandler
+    {
+
+    }
+}
