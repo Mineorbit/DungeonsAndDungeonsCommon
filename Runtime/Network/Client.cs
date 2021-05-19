177500bae49513296119a0d90f84c89e6f533185
mineorbit <mineorbitbeta@gmail.com>
2021-05-19 23:20:06 +0200

    fix attempt

--- Runtime/Network/Client.cs
+++ Runtime/Network/Client.cs
@@ -317,7 +317,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             }
             else
             {
-                int offset = !isOnServer ? 4 : 0;
+                int offset = isOnServer ? 4 : 0;
                 IPEndPoint r = remote;
                 r.Port = r.Port + offset;
                 Debug.Log("Reading for UDP on " + r);
