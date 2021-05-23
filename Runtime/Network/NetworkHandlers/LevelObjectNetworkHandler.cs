73d09e17f025f32491a692cb0f75af78a4ef0971
mineorbit <maximilian.stillger@arcor.de>
2021-05-23 15:27:51 +0200

    fix

--- Runtime/Network/NetworkHandlers/LevelObjectNetworkHandler.cs
+++ Runtime/Network/NetworkHandlers/LevelObjectNetworkHandler.cs
@@ -70,7 +70,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             arguments.Add(packedArgument.Item1, packedArgument.Item2);
 
             action.Params.Add(arguments);
-            Marshall(action);
+            Marshall(action,TCP: false);
         }
 
 
@@ -80,7 +80,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             {
                 ActionName = actionName
             };
-            Marshall(action);
+            Marshall(action,TCP: false);
         }
 
 
