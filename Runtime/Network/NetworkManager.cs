8070169ac26bde0896b3a71e8dfd2828407b3988
mineorbit <mineorbitbeta@gmail.com>
2021-06-22 00:11:03 +0200

    fix

--- Runtime/Network/NetworkManager.cs
+++ Runtime/Network/NetworkManager.cs
@@ -48,7 +48,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 
 
             // THIS IS A STUPID PLACE BUT WILL CHANGE LATER
-            Time.fixedDeltaTime = 0.03125f;
+            Time.fixedDeltaTime = 0.02f;
 
             foreach (var p in packetBindings) p.AddToBinding();
             
