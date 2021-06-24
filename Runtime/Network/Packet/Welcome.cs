d5337533e12dd7df93c7c7d3999f4fa63d09a30b
mineorbit <mineorbitbeta@gmail.com>
2021-06-24 16:38:14 +0200

    Fix

--- Runtime/Network/Packet/Welcome.cs
+++ Runtime/Network/Packet/Welcome.cs
@@ -24,12 +24,14 @@ namespace State {
     static WelcomeReflection() {
       byte[] descriptorData = global::System.Convert.FromBase64String(
           string.Concat(
-            "Cg1XZWxjb21lLnByb3RvEgVzdGF0ZSInCgdXZWxjb21lEg8KB2xvY2FsSWQY",
-            "ASABKAUSCwoDdWRwGAIgASgFYgZwcm90bzM="));
+            "Cg1XZWxjb21lLnByb3RvEgVzdGF0ZSIaCgdXZWxjb21lEg8KB2xvY2FsSWQY",
+            "ASABKAUiMwoPV2VsY29tZVVuc3RhYmxlEg8KB21lc3NhZ2UYASABKAkSDwoH",
+            "dWRwUG9ydBgCIAEoBWIGcHJvdG8z"));
       descriptor = pbr::FileDescriptor.FromGeneratedCode(descriptorData,
           new pbr::FileDescriptor[] { },
           new pbr::GeneratedClrTypeInfo(null, null, new pbr::GeneratedClrTypeInfo[] {
-            new pbr::GeneratedClrTypeInfo(typeof(global::State.Welcome), global::State.Welcome.Parser, new[]{ "LocalId", "Udp" }, null, null, null, null)
+            new pbr::GeneratedClrTypeInfo(typeof(global::State.Welcome), global::State.Welcome.Parser, new[]{ "LocalId" }, null, null, null, null),
+            new pbr::GeneratedClrTypeInfo(typeof(global::State.WelcomeUnstable), global::State.WelcomeUnstable.Parser, new[]{ "Message", "UdpPort" }, null, null, null, null)
           }));
     }
     #endregion
@@ -66,7 +68,6 @@ namespace State {
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public Welcome(Welcome other) : this() {
       localId_ = other.localId_;
-      udp_ = other.udp_;
       _unknownFields = pb::UnknownFieldSet.Clone(other._unknownFields);
     }
 
@@ -86,17 +87,6 @@ namespace State {
       }
     }
 
-    /// <summary>Field number for the "udp" field.</summary>
-    public const int UdpFieldNumber = 2;
-    private int udp_;
-    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
-    public int Udp {
-      get { return udp_; }
-      set {
-        udp_ = value;
-      }
-    }
-
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
     public override bool Equals(object other) {
       return Equals(other as Welcome);
@@ -111,7 +101,6 @@ namespace State {
         return true;
       }
       if (LocalId != other.LocalId) return false;
-      if (Udp != other.Udp) return false;
       return Equals(_unknownFields, other._unknownFields);
     }
 
@@ -119,7 +108,6 @@ namespace State {
     public override int GetHashCode() {
       int hash = 1;
       if (LocalId != 0) hash ^= LocalId.GetHashCode();
-      if (Udp != 0) hash ^= Udp.GetHashCode();
       if (_unknownFields != null) {
         hash ^= _unknownFields.GetHashCode();
       }
@@ -140,10 +128,6 @@ namespace State {
         output.WriteRawTag(8);
         output.WriteInt32(LocalId);
       }
-      if (Udp != 0) {
-        output.WriteRawTag(16);
-        output.WriteInt32(Udp);
-      }
       if (_unknownFields != null) {
         _unknownFields.WriteTo(output);
       }
@@ -157,10 +141,6 @@ namespace State {
         output.WriteRawTag(8);
         output.WriteInt32(LocalId);
       }
-      if (Udp != 0) {
-        output.WriteRawTag(16);
-        output.WriteInt32(Udp);
-      }
       if (_unknownFields != null) {
         _unknownFields.WriteTo(ref output);
       }
@@ -172,9 +152,6 @@ namespace State {
       int size = 0;
       if (LocalId != 0) {
         size += 1 + pb::CodedOutputStream.ComputeInt32Size(LocalId);
-      }
-      if (Udp != 0) {
-        size += 1 + pb::CodedOutputStream.ComputeInt32Size(Udp);
       }
       if (_unknownFields != null) {
         size += _unknownFields.CalculateSize();
@@ -189,9 +166,6 @@ namespace State {
       }
       if (other.LocalId != 0) {
         LocalId = other.LocalId;
-      }
-      if (other.Udp != 0) {
-        Udp = other.Udp;
       }
       _unknownFields = pb::UnknownFieldSet.MergeFrom(_unknownFields, other._unknownFields);
     }
@@ -209,10 +183,6 @@ namespace State {
             break;
           case 8: {
             LocalId = input.ReadInt32();
-            break;
-          }
-          case 16: {
-            Udp = input.ReadInt32();
             break;
           }
         }
@@ -233,8 +203,212 @@ namespace State {
             LocalId = input.ReadInt32();
             break;
           }
+        }
+      }
+    }
+    #endif
+
+  }
+
+  public sealed partial class WelcomeUnstable : pb::IMessage<WelcomeUnstable>
+  #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+      , pb::IBufferMessage
+  #endif
+  {
+    private static readonly pb::MessageParser<WelcomeUnstable> _parser = new pb::MessageParser<WelcomeUnstable>(() => new WelcomeUnstable());
+    private pb::UnknownFieldSet _unknownFields;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public static pb::MessageParser<WelcomeUnstable> Parser { get { return _parser; } }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public static pbr::MessageDescriptor Descriptor {
+      get { return global::State.WelcomeReflection.Descriptor.MessageTypes[1]; }
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    pbr::MessageDescriptor pb::IMessage.Descriptor {
+      get { return Descriptor; }
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public WelcomeUnstable() {
+      OnConstruction();
+    }
+
+    partial void OnConstruction();
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public WelcomeUnstable(WelcomeUnstable other) : this() {
+      message_ = other.message_;
+      udpPort_ = other.udpPort_;
+      _unknownFields = pb::UnknownFieldSet.Clone(other._unknownFields);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public WelcomeUnstable Clone() {
+      return new WelcomeUnstable(this);
+    }
+
+    /// <summary>Field number for the "message" field.</summary>
+    public const int MessageFieldNumber = 1;
+    private string message_ = "";
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public string Message {
+      get { return message_; }
+      set {
+        message_ = pb::ProtoPreconditions.CheckNotNull(value, "value");
+      }
+    }
+
+    /// <summary>Field number for the "udpPort" field.</summary>
+    public const int UdpPortFieldNumber = 2;
+    private int udpPort_;
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public int UdpPort {
+      get { return udpPort_; }
+      set {
+        udpPort_ = value;
+      }
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public override bool Equals(object other) {
+      return Equals(other as WelcomeUnstable);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public bool Equals(WelcomeUnstable other) {
+      if (ReferenceEquals(other, null)) {
+        return false;
+      }
+      if (ReferenceEquals(other, this)) {
+        return true;
+      }
+      if (Message != other.Message) return false;
+      if (UdpPort != other.UdpPort) return false;
+      return Equals(_unknownFields, other._unknownFields);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public override int GetHashCode() {
+      int hash = 1;
+      if (Message.Length != 0) hash ^= Message.GetHashCode();
+      if (UdpPort != 0) hash ^= UdpPort.GetHashCode();
+      if (_unknownFields != null) {
+        hash ^= _unknownFields.GetHashCode();
+      }
+      return hash;
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public override string ToString() {
+      return pb::JsonFormatter.ToDiagnosticString(this);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public void WriteTo(pb::CodedOutputStream output) {
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+      output.WriteRawMessage(this);
+    #else
+      if (Message.Length != 0) {
+        output.WriteRawTag(10);
+        output.WriteString(Message);
+      }
+      if (UdpPort != 0) {
+        output.WriteRawTag(16);
+        output.WriteInt32(UdpPort);
+      }
+      if (_unknownFields != null) {
+        _unknownFields.WriteTo(output);
+      }
+    #endif
+    }
+
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    void pb::IBufferMessage.InternalWriteTo(ref pb::WriteContext output) {
+      if (Message.Length != 0) {
+        output.WriteRawTag(10);
+        output.WriteString(Message);
+      }
+      if (UdpPort != 0) {
+        output.WriteRawTag(16);
+        output.WriteInt32(UdpPort);
+      }
+      if (_unknownFields != null) {
+        _unknownFields.WriteTo(ref output);
+      }
+    }
+    #endif
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public int CalculateSize() {
+      int size = 0;
+      if (Message.Length != 0) {
+        size += 1 + pb::CodedOutputStream.ComputeStringSize(Message);
+      }
+      if (UdpPort != 0) {
+        size += 1 + pb::CodedOutputStream.ComputeInt32Size(UdpPort);
+      }
+      if (_unknownFields != null) {
+        size += _unknownFields.CalculateSize();
+      }
+      return size;
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public void MergeFrom(WelcomeUnstable other) {
+      if (other == null) {
+        return;
+      }
+      if (other.Message.Length != 0) {
+        Message = other.Message;
+      }
+      if (other.UdpPort != 0) {
+        UdpPort = other.UdpPort;
+      }
+      _unknownFields = pb::UnknownFieldSet.MergeFrom(_unknownFields, other._unknownFields);
+    }
+
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    public void MergeFrom(pb::CodedInputStream input) {
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+      input.ReadRawMessage(this);
+    #else
+      uint tag;
+      while ((tag = input.ReadTag()) != 0) {
+        switch(tag) {
+          default:
+            _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, input);
+            break;
+          case 10: {
+            Message = input.ReadString();
+            break;
+          }
           case 16: {
-            Udp = input.ReadInt32();
+            UdpPort = input.ReadInt32();
+            break;
+          }
+        }
+      }
+    #endif
+    }
+
+    #if !GOOGLE_PROTOBUF_REFSTRUCT_COMPATIBILITY_MODE
+    [global::System.Diagnostics.DebuggerNonUserCodeAttribute]
+    void pb::IBufferMessage.InternalMergeFrom(ref pb::ParseContext input) {
+      uint tag;
+      while ((tag = input.ReadTag()) != 0) {
+        switch(tag) {
+          default:
+            _unknownFields = pb::UnknownFieldSet.MergeFieldFrom(_unknownFields, ref input);
+            break;
+          case 10: {
+            Message = input.ReadString();
+            break;
+          }
+          case 16: {
+            UdpPort = input.ReadInt32();
             break;
           }
         }
