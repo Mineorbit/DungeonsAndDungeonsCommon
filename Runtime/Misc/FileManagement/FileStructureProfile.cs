a787be9f7680ca5464dec0c91e21fda1d78ee116
mineorbit <maximilian.stillger@arcor.de>
2021-07-13 13:05:38 +0200

    fix

--- Runtime/Misc/FileManagement/FileStructureProfile.cs
+++ Runtime/Misc/FileManagement/FileStructureProfile.cs
@@ -33,7 +33,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         {
             if (parent == null)
                 return Application.persistentDataPath + "/" + fileStructureProfileName + "/";
-            string path = fileStructureProfileName + parent.GetPath() + fileStructureProfileName;
+            string path = parent.GetPath() + fileStructureProfileName;
             FileManager.createFolder(path, false);
             return path + "/";;
         }
