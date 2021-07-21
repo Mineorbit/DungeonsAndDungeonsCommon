6c74d37d3b442b77e85b8418843602382a00f0e3
mineorbit <mineorbitbeta@gmail.com>
2021-07-21 14:53:59 +0200

    fix

---
+++ Runtime/Audio/BlogAudioController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace com.mineorbit.dungeonsanddungeonscommon
+{
+    	public class BlogAudioController : EnemyAudioController
+    	{
+    // Start is called before the first frame update
+    		void Start()
+    		{
+        
+    		}
+		public void Footstep()
+		{
+		Play(0);
+		}
+    // Update is called once per frame
+    		void Update()
+   		{
+        
+    		}
+	}
+}
