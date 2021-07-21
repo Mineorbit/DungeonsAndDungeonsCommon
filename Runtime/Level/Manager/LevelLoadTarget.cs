cffe0da601f5caac90bf97059587a4e519938a24
mineorbit <mineorbitbeta@gmail.com>
2021-07-21 15:42:27 +0200

    fix

--- Runtime/Level/Manager/LevelLoadTarget.cs
+++ Runtime/Level/Manager/LevelLoadTarget.cs
@@ -3,7 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using NetLevel;
-using PlasticGui.Help.NewVersions;
 using UnityEngine;
 
 namespace com.mineorbit.dungeonsanddungeonscommon
