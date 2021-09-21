e1dc658aeecd84c07fcd2dbbd80af274d8fe3448
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2021-09-21 15:34:34 +0200

    fixed handling

--- Runtime/Level/Structure/LevelObject/Item/Bow.cs
+++ Runtime/Level/Structure/LevelObject/Item/Bow.cs
@@ -6,12 +6,12 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 {
     public class Bow : Item
     {
-        // Start is called before the first frame update
-        void Start()
+        public override void OnAttach()
         {
-
+            base.OnAttach();
+            transform.localPosition = new Vector3(0, 0.002f, 0);
+            transform.localEulerAngles = new Vector3(0, 0, 0);
         }
-
         // Update is called once per frame
         void Update()
         {
