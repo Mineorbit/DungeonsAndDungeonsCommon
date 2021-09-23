1e353c5b1e87532c3ac6a51c4b643b1e73810702
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2021-09-23 15:21:54 +0200

    fix

--- Runtime/Level/Structure/LevelObject/Entity/Arrow.cs
+++ Runtime/Level/Structure/LevelObject/Entity/Arrow.cs
@@ -54,6 +54,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 
             Vector3 target = hit.point;
             Vector3 dir = target - transform.position;
+            Debug.DrawRay(transform.position,dir,Color.green);
             return Quaternion.LookRotation(dir,Vector3.up);
         }
         
