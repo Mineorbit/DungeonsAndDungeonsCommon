338036517435cb6fe458a9acb357397ff0e5961f
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2021-10-06 14:44:35 +0200

    fix

--- Runtime/Level/Structure/LevelObject/Entity/Arrow.cs
+++ Runtime/Level/Structure/LevelObject/Entity/Arrow.cs
@@ -50,7 +50,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         
         Quaternion GetAimDirection()
         {
-            Vector3 target = PlayerController.GetTargetPoint();
+            Vector3 target = ((PlayerController)shootingBow.owner.controller).GetTargetPoint();
             //LevelManager.currentLevel.AddDynamic(test, target, Quaternion.identity, null);
             Vector3 dir = target - transform.position;
             lastTarget = target;
