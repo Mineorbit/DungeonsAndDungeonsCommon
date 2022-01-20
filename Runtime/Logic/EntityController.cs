8e3c692382fe59656d0d7365e9cd0523a2961c9a
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-01-20 16:15:17 +0100

    fix

--- Runtime/Logic/EntityController.cs
+++ Runtime/Logic/EntityController.cs
@@ -40,26 +40,44 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             }
         }
 
-        /*
-        public IEnumerator KickbackRoutine(Vector3 dir, float dist, float kickbackSpeed)
+        public virtual void Deactivate()
         {
-            float t = 0;
-            Vector3 start = transform.position;
-            float kickbackTime =  dist / kickbackSpeed;
-            float speedY = 0;
-            while (t<kickbackTime)
+            enabled = false;
+            if (controller != null)
             {
-                t += Time.deltaTime;
-                if (!controller.isGrounded)
-                {
-                    speedY += Time.deltaTime * gravity;
-                }
-                controller.Move(0.05f*kickbackSpeed*dir+speedY*Vector3.down);
-                yield return new WaitForEndOfFrame();
+                controller.enabled = false;
             }
-            entity.FinishKickback();
         }
-        */
+
+        public virtual void Activate()
+        {
+            enabled = true;
+            if (controller != null)
+            {
+                controller.enabled = true;
+            }
+        }
+
+            /*
+            public IEnumerator KickbackRoutine(Vector3 dir, float dist, float kickbackSpeed)
+            {
+                float t = 0;
+                Vector3 start = transform.position;
+                float kickbackTime =  dist / kickbackSpeed;
+                float speedY = 0;
+                while (t<kickbackTime)
+                {
+                    t += Time.deltaTime;
+                    if (!controller.isGrounded)
+                    {
+                        speedY += Time.deltaTime * gravity;
+                    }
+                    controller.Move(0.05f*kickbackSpeed*dir+speedY*Vector3.down);
+                    yield return new WaitForEndOfFrame();
+                }
+                entity.FinishKickback();
+            }
+            */
         
         public void OnSpawn(Vector3 location)
         {
