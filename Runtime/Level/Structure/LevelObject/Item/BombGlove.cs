f9d80c299d82c815ae86ee65cf694f89533d0471
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-01-21 23:05:28 +0100

    bomb throw

--- Runtime/Level/Structure/LevelObject/Item/BombGlove.cs
+++ Runtime/Level/Structure/LevelObject/Item/BombGlove.cs
@@ -1,17 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Assertions.Must;
 
 namespace com.mineorbit.dungeonsanddungeonscommon
 {
     public class BombGlove : Item
     {
-        // Start is called before the first frame update
+        public LevelObjectData bomb;
+        public float bombThrowTime;
+        public bool canThrowBomb;
         void Start()
         {
-
+            canThrowBomb = true;
         }
 
+        public override void Use()
+        {
+            base.Use();
+            if(canThrowBomb)
+            {
+                canThrowBomb = false;
+                Invoke(CreateBomb,false,true);
+                Invoke("AllowThrow",bombThrowTime);
+            }
+        }
+
+        public void AllowThrow()
+        {
+            canThrowBomb = true;
+        }
+        
+        public void CreateBomb()
+        {
+                Vector3 forward = owner.transform.forward;
+                GameConsole.Log("Creating Arrow");
+                GameObject arrowObject = LevelManager.currentLevel.AddDynamic(bomb,transform.position + 0.5f*forward, Quaternion.LookRotation(Vector3.forward), new Util.Optional<int>());
+                arrowObject.GetComponent<Rigidbody>().AddForce(forward);
+                
+        }
         // Update is called once per frame
         void Update()
         {
