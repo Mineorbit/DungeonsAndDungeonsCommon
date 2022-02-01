c05965af0618db4feb15c8f2161661d97822c28f
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-01 23:44:47 +0100

    fix

--- Runtime/Network/NetworkHandlers/ItemNetworkHandler.cs
+++ Runtime/Network/NetworkHandlers/ItemNetworkHandler.cs
@@ -9,36 +9,12 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         public override void Start()
         {
             base.Start();
-            transmitPosition = true;
-            interpolatePosition = true;
-
-            bool server = NetworkManager.instance.localId == -1;
-            GetObservedItem().rigidbody.isKinematic = !server;
-            GetObservedItem().rigidbody.useGravity = server;
             
-            GetObservedItem().onAttachEvent.AddListener(() => { owner = ((Player) GetObservedItem().owner).localId; 
-                transmitPosition = false;
-                interpolatePosition = false;
-            });
             
-            GetObservedItem().onDettachEvent.AddListener(() => { owner = -1; 
-                transmitPosition = true;
-                interpolatePosition = true;
-                GetObservedItem().rigidbody.isKinematic = !server;
-                GetObservedItem().rigidbody.useGravity = server;
-            });
 
         }
 
-        public override bool WantsToTransmit()
-        {
-            return owner == NetworkManager.instance.localId;
-        }
         
-        public Item GetObservedItem()
-        {
-            return (Item) GetObservedEntity();
-        }
        
     }
 }
