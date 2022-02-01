d8b3435c94e5bca2b4587570165cc1e8d33baf00
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-01 18:57:16 +0100

    fix

--- Runtime/GameConsole.cs
+++ Runtime/GameConsole.cs
@@ -59,11 +59,13 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         {
             string fps = $"FPS: {(1 / Time.deltaTime)}\n";
             string appendage = $"Received Packets: {Client.receivedPacketCarriers}\n" +
-                            $"Handled Packets: {Client.handledPackets}\n" +
-                            $"Sent Packets: {Client.sentPacketCarriers} \n" +
-                            $"UDP Queue Length: {NetworkManager.instance.client.packetOutUDPBuffer.Count} \n" +
+                               $"Handled Packets: {Client.handledPackets}\n" +
+                               $"Sent Packets: {Client.sentPacketCarriers} \n";
+            if(NetworkManager.instance.client != null)
+            {
+            appendage += "UDP Queue Length: {NetworkManager.instance.client.packetOutUDPBuffer.Count} \n" +
                             $"TCP Queue Length: {NetworkManager.instance.client.packetOutTCPBuffer.Count}";
-
+            }
             infoText.text = fps + appendage;
         }
 
