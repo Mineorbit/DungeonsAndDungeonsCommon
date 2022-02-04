18374b840836786e2ac79fc64d4d886c6389e189
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-04 22:18:52 +0100

    fix attempt

--- Runtime/Network/NetworkHandlers/PlayerNetworkHandler.cs
+++ Runtime/Network/NetworkHandlers/PlayerNetworkHandler.cs
@@ -24,6 +24,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         
         public void UpdateInputData()
         {
+            GameConsole.Log($"Updating Input Data for {GetObservedPlayer().localId}");
             Message input = Message.Create(MessageSendMode.unreliable,(ushort) NetworkManager.ClientToServerId.playerInput);
             PlayerController c = (PlayerController) GetObservedPlayer().controller;
             input.AddVector3(c.movingDirection);
