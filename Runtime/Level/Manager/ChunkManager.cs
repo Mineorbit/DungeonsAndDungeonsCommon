efb4d27f263f8c28f9861a8003196aab2b009308
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-04 14:35:23 +0100

    fix

--- Runtime/Level/Manager/ChunkManager.cs
+++ Runtime/Level/Manager/ChunkManager.cs
@@ -24,7 +24,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 
         public static ChunkManager instance;
 
-        public static float chunkGranularity = 9;
+        public static float chunkGranularity = 8;
 
         public static int regionGranularity = 2;
 
