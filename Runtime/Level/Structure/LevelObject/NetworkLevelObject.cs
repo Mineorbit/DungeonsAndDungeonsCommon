1c48d8f0fedf32f01742b910bf6c08f2f868083c
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-04 15:59:16 +0100

    fix

--- Runtime/Level/Structure/LevelObject/NetworkLevelObject.cs
+++ Runtime/Level/Structure/LevelObject/NetworkLevelObject.cs
@@ -104,7 +104,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         
         public virtual void Invoke<T>(Action<T> a, T argument, bool doClient = true, bool doServer = true)
         {
-	        if (doServer && NetworkManager.instance.isOnServer || !NetworkManager.instance.isOnServer && doClient)
+	        if (Level.instantiateType == Level.InstantiateType.Test || doServer && NetworkManager.instance.isOnServer || !NetworkManager.instance.isOnServer && doClient)
 	        {
 		        a.DynamicInvoke(argument);
 	        }
