c27bf68ea2a1f36c16a3ff1e1aaadb426c30ce5c
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-05 21:04:33 +0100

    fix

--- Runtime/Network/NetworkHandlers/LevelLoadTargetNetworkHandler.cs
+++ Runtime/Network/NetworkHandlers/LevelLoadTargetNetworkHandler.cs
@@ -56,6 +56,13 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             Chunk chunk = ChunkManager.GetChunkByID(chunkData.ChunkId);
             Message[,,] messages = new Message[2,2,2];
             byte[,,][] datas = new byte[2, 2, 2][];
+            for(int i = 0; i < 2;i++)
+            for(int j = 0; j < 2;j++)
+            for (int k = 0; k < 2; k++)
+            {
+                datas[i, j, k] = new byte[1024];
+            }
+
             foreach (Transform levelObject in chunk.transform)
             {
                 Vector3 pos = levelObject.transform.localPosition;
