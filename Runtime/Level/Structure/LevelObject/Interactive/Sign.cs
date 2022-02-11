42e271bed191c27bf23883663cf9755a6c4de249
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-02-11 03:45:34 +0100

    fix

--- Runtime/Level/Structure/LevelObject/Interactive/Sign.cs
+++ Runtime/Level/Structure/LevelObject/Interactive/Sign.cs
@@ -9,9 +9,10 @@ namespace com.mineorbit.dungeonsanddungeonscommon
 {
     public class Sign : InteractiveLevelObject
     {
-        public TextMeshPro textMesh;
+        public TextMeshProUGUI textMesh;
         public void Start()
         {
+            textMesh = transform.GetComponent<TextMeshProUGUI>();
             GetProperty("text").valueChangedHandler += (a, b) =>
             {
                 textMesh.SetText(GetProperty("text").Value);
