02c2be4cfe9f33bf11dc7b82811a2c994e61aecc
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-03-19 03:30:20 +0100

    fix

--- Runtime/Network/NetworkHandlers/LevelLoadTargetNetworkHandler.cs
+++ Runtime/Network/NetworkHandlers/LevelLoadTargetNetworkHandler.cs
@@ -63,13 +63,8 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             }
             receivedChunkFragments.Add(fragment);
 
-            byte[] data = new byte[1024];
-            for (int i = 0; i < data.Length; i++)
-            {
-                data[i] = m.GetByte();
-            }
-
-            //byte[] data = m.GetBytes(isBigArray: true);
+            
+            byte[] data = m.GetBytes(isBigArray: true);
             Vector3 offset = new Vector3(chunkX * 8, chunkY * 8, chunkZ * 8);
             for(int x = 0;x<8;x++)
             for(int y = 0;y<8;y++)
@@ -196,10 +191,10 @@ namespace com.mineorbit.dungeonsanddungeonscommon
                         byte[] data = new byte[1024];
                         for (int h = 0; h < 1024; h++)
                         {
-                           // data[h] = datas[i, j, k, h];
-                           message.AddByte(datas[i, j, k, h]);
+                            data[h] = datas[i, j, k, h];
+                           //message.AddByte(datas[i, j, k, h]);
                         }
-                        //message.AddBytes(data,isBigArray:true);
+                        message.AddBytes(data,isBigArray:true);
                         //GameConsole.Log($"Bytes: {PrintByteArray(data)}");
                         messages.Add(message);
             
