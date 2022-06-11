9ca1d35a390ca51652644e69b76e164a8f61c977
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-06-11 17:08:37 +0200

    fix

--- Runtime/Level/Structure/LevelObject/Interactive/Spike.cs
+++ Runtime/Level/Structure/LevelObject/Interactive/Spike.cs
@@ -26,7 +26,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
             buildCollider.enabled = false;
             Hitbox.Attach("Entity");
             Hitbox.enterEvent.AddListener((x)=>TryDamage(x));
-            Activate();
+            Deactivate();
         }
 
         private void TryDamage(GameObject g)
@@ -42,13 +42,6 @@ namespace com.mineorbit.dungeonsanddungeonscommon
                     c.Hit( this, spikeDamage);
                 }
             }
-        }
-
-
-        public override void OnEndRound()
-        {
-            base.OnEndRound();
-            Deactivate();
         }
         
         public override void Activate()
@@ -69,7 +62,7 @@ namespace com.mineorbit.dungeonsanddungeonscommon
         {
             base.ResetState();
             buildCollider.enabled = true;
-            Activate();
+            Deactivate();
         }
     }
 }
