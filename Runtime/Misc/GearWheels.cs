f25889bee568a131ea769edd838ea8555b98cfb8
Maximilian Stillger <maximilian.stillger@stud.tu-darmstadt.de>
2022-06-14 00:13:24 +0200

    fix

--- Runtime/Misc/GearWheels.cs
+++ Runtime/Misc/GearWheels.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using com.mineorbit.dungeonsanddungeonscommon;
 using UnityEngine;
 
 public class GearWheels : MonoBehaviour
@@ -8,13 +9,14 @@ public class GearWheels : MonoBehaviour
     private float[] speeds = new float[4];
     private float t = 0;
 
-    public float maxSpeed = 4f;
+    public float maxSpeed = 8f;
     // Start is called before the first frame update
     void Start()
     {
         for (int i = 0; i < 4; i++)
         {
             speeds[i] = Random.Range(0f,maxSpeed);
+            gearwheels[i].GetComponent<MeshRenderer>().sharedMaterial.color = Constants.ToColor((Constants.Color) i);
         }
     }
 
